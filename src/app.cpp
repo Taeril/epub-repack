73233ce48f8293cc969acb5cb5ce4ce4fa6d0127
Taeril <taeril@kraina.org>
2023-04-09 17:25:20 +0200

    Add <filesystem> header and set output from pattern
    Use FindFilesystem.cmake from CMake Community Modules
    https://github.com/vector-of-bool/CMakeCM
    File downloaded from URL:
    https://github.com/vector-of-bool/CMakeCM/raw/master/modules/FindFilesystem.cmake

--- src/app.cpp
+++ src/app.cpp
@@ -1,9 +1,41 @@
 #include "app.hpp"
+#include "filesystem.hpp"
+
+static void replace_all(std::string& str, std::string const& from, std::string const& to) {
+    std::string::size_type pos = 0;
+    while((pos = str.find(from, pos)) != std::string::npos) {
+        str.replace(pos, from.length(), to);
+        pos += to.length() - from.length() + 1;
+    }
+}
 
 int App::run(int argc, char** argv) {
 	int ret = args(argc, argv);
 	if(ret != 0) return ret;
 
+	for(auto const& file : files_) {
+		xprint(1, "File: {}\n", file);
+
+		fs::path p(file);
+		std::string filename = p.filename().string();
+		std::string name = p.stem().string();
+		std::string ext = p.extension().string();
+		std::string dir = p.parent_path().string();
+		if(dir.empty()) dir = ".";
+
+		std::string output(output_pattern_);
+		replace_all(output, "{DIR}", dir);
+		replace_all(output, "{FILENAME}", filename);
+		replace_all(output, "{NAME}", name);
+		replace_all(output, "{EXT}", ext);
+
+
+		xprint(1, " - DIR: {}\n", dir);
+		xprint(1, " - FILENAME: {}\n", filename);
+		xprint(1, " - NAME: {}\n", name);
+		xprint(1, " - EXT: {}\n", ext);
+		xprint(1, " => {}\n", output);
+	}
 
 	return 0;
 }
